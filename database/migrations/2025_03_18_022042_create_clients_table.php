f4a0cad89a0ffe27ed1ad8c4d9797912371f727c
randaru09 <ndaruanggoro123@gmail.com>
2025-03-18 09:26:26 +0700

    feat: add some col in migration

--- database/migrations/2025_03_18_022042_create_clients_table.php
+++ database/migrations/2025_03_18_022042_create_clients_table.php
@@ -4,16 +4,25 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::create('clients', function (Blueprint $table) {
-            $table->id();
+        Schema::create('my_client', function (Blueprint $table) {
+            $table->id()->primary();
+            $table->string('name', 250);
+            $table->string('slug', 100);
+            $table->enum('is_project', ['0', '1'])->default('0');
+            $table->char('self_capture', 1)->default('1');
+            $table->char('client_prefix', 4);
+            $table->string('client_logo', 255)->default('no-image.jpg');
+            $table->text('address')->nullable();
+            $table->string('phone_number', 50)->nullable();
+            $table->string('city', 50)->nullable();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
